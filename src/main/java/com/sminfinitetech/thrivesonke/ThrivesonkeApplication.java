8b1f67db948d53a50bce59df321f72416ddf4f6f
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2024-11-25 12:26:46 +0200

    first commit

---
+++ src/main/java/com/sminfinitetech/thrivesonke/ThrivesonkeApplication.java
@@ -0,0 +1,12 @@
+package com.sminfinitetech.thrivesonke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrivesonkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrivesonkeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sminfinitetech/thrivesonke/ThrivesonkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sminfinitetech.thrivesonke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrivesonkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
